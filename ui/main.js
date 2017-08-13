e6fec8d697647d826374e7ee6448dd24862463b3
bhalajisivakumar <30322201+bhalajisivakumar@users.noreply.github.com>
2017-08-13 18:33:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,5 +4,6 @@ var element = document.getElementById('main-text'
 element.innerHTML = 'NEW VALUE';
 var img = document.getElementById('madi');
 img.onclick = function() {
-    img.style.marginLeft = '100px';
+    
+    var interval = setInterval(moveLeft,100);
 }
