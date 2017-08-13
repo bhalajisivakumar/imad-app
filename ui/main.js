268f5d13fda98d0c3e2011f16340d4e53eb41e73
bhalajisivakumar <30322201+bhalajisivakumar@users.noreply.github.com>
2017-08-13 18:27:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,5 @@
 console.log('Loaded!');
-document.getElementById('main-text'
+var element = document.getElementById('main-text'
 );
 element.innerHTML = 'NEW VALUE';
 var img = document.getElementById('madi');
