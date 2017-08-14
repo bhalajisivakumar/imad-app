f810e49f1bb6e13a3545ba05458554d12c89f3cf
bhalajisivakumar <30322201+bhalajisivakumar@users.noreply.github.com>
2017-08-14 16:49:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -63,9 +63,9 @@
 // //////////////////////////////////////////////////////////////
     //SUBMIT COMMENT
 
-        var submit2 = document.getElementById('submitButton');
+        var submit = document.getElementById('submitButton');
         //after cloicking it should display the comments
-        submit2.onclick = function() {
+        submit.onclick = function() {
         //create a request object
         var request = new XMLHttpRequest();
         //Capture the reponse and store it ina variable
