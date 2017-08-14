c514c65f4e6e9d745da89f095b84592614d14c56
bhalajisivakumar <30322201+bhalajisivakumar@users.noreply.github.com>
2017-08-14 12:37:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,6 +21,9 @@ var articles = {
         <p>
              Content for my file article one......   Content for my file article one......   Content for my file article one......   Content for my file article one......   Content for my file article one......   Content for my file article one......  
         </p>`
+        
+        //add a comments box
+        <input type = "text" id ="comments" placeholder="comments" ></input>
 },
    'article-two' : {
             title:'Article Two | Bhalaji Sivakumar',
