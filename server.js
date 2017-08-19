f865ae309ddf0abd2a24d214d082eba7f160a93d
bhalajisivakumar <30322201+bhalajisivakumar@users.noreply.github.com>
2017-08-19 14:00:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -108,8 +108,15 @@ app.get('/', function (req, res) {
 });
 // ////////// db /////////
 // make arequest
+var pool = new Pool(config);
 app.get(':/test-db',function(req,res) {
-    
+    pool.query('SELECT * FROM test',function(req,res) {
+        if(err) {
+            res.status(500).send(err.toString());
+        } else {
+            res.send(JSON.stringify(result));
+        }
+        });
 });
 //response
 // /////////// COUNTER ///////////////////////////////////
