f4bb1100772e0d2b7a70144ae37d5139fe730a37
bhalajisivakumar <30322201+bhalajisivakumar@users.noreply.github.com>
2017-09-03 19:25:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,29 +1,5 @@
-// //////////// countere code///////////////////////////
-        var button= document.getElementById('counter');
-        
-        button.onclick = function () {
-        
-        //create a request to the counter endpoint
-        var request = new XMLHttpRequest();
-        
-        //Capture the reponse and store it ina variable
-        request.onreadystatechange = function () {
-        if (request.readyState === XMLHttpRequest.DONE) {
-        //TAKE SOME ACTION
-        if(request.status === 200) {
-        var counter = request.responseText;
-        var span = document.getElementById('count');
-        span.innerHTML = counter.toString();
-        }
-        }
-        //Render the variable in a corect span
-        };
-        //make the request 
-        request.open('GET','http://balajisk1996.imad.hasura-app.io/counter',true);
-        request.send(null);
-        };
-// /////////////////////////////////////////////////////////////////////
-            //SUBMIT NAME
+
+            //SUBMIT username/password to login
             
                 var submit = document.getElementById('submit_btn');
                 submit.onclick = function () {
@@ -39,23 +15,27 @@
                 if (request.readyState === XMLHttpRequest.DONE) {
                 //TAKE SOME ACTION
                 if(request.status === 200) {
-                var names = request.responseText;
-                names = JSON.parse(names);
-                var list = ' ';
-                for(var i = 0; i < names.length; i++) {
-                list += '<li>' + names[i] + '</li>';
-                }
-                var ul = document.getElementById('namelist');
-                ul.innerHTML = list;
-                }
-                }
+               console.log('user logged in');
+               alert('logged in successfully');
+               
+                } else if (request.status === 403) {
+                    alert('Username/password is incorrect');
+                         } else if (request.status === 500) {
+                             alert('Something went wrong on the server');
+                         }
                 
-                };
+                     }
+                  };
+                
+             };
                 //make the request 
-                var nameInput = document.getElementById('name');
-                var name = nameInput.value; 
-                request.open('GET','http://balajisk1996.imad.hasura-app.io/submit-name?name='+ name ,true);
-                request.send(null);
+                var username = document.getElementById('username').value;
+                var password = document.getElementById('password').value;
+                console.log(username);
+                console.log(password);
+                request.open('POST','http://balajisk1996.imad.hasura-app.io/login', true);
+                request.setRequestHeader('Content-Type', 'application/json');
+                request.send(JSON.stringify({username: username, password: password}));
                 //capture a list of namwes
                 };
                 
