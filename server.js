0ab02f38fb273b738b2a54c76982a7af02d2cb3d
bhalajisivakumar <30322201+bhalajisivakumar@users.noreply.github.com>
2017-09-05 10:55:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ var bodyParser = require('body-parser');
 var session = require('express-session');
 
 var config = {
-    user: 'coco98',
-    database: 'coco98',
+    user: 'balajisk1996',
+    database: 'balajisk1996',
     host: 'db.imad.hasura-app.io',
     port: '5432',
     password: process.env.DB_PASSWORD
